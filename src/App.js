0bb903d24958aae00cd91ea2e04261cb7bb5425e
Alyssa Hooper <nesteralyssa@gmail.com>
2020-04-28 17:12:00 -0700

    exports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Greet from './components/Greet';
 
 class App extends Component {
   render () {
     return (
       <div className="landing">
         <h1>I'M WORKINGGGG.</h1>
+        <Greet />
       </div>
     );
   }
